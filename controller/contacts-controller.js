b6f28995891c9502c3125c6c73d5abe08d585a3a
Ania <124797307+aniakurtiak@users.noreply.github.com>
2023-11-29 17:01:14 +0200

    add decorators

--- controller/contacts-controller.js
+++ controller/contacts-controller.js
@@ -1,64 +1,41 @@
 import contactService from "../models/contacts.js";
 import { HttpError } from "../helpers/index.js";
+import { ctrlWrapper } from "../decorators/index.js";
 
 import {
   addContactSchema,
   updateContactSchema,
 } from "../schemas/contacts-schema.js";
 
-const getAllContacts = async (req, res, next) => {
-  try {
-    const contacts = await contactService.listContacts();
-    res.json(contacts);
-  } catch (error) {
-    next(error);
-  }
+const getAllContacts = async (req, res) => {
+  const contacts = await contactService.listContacts();
+  res.json(contacts);
 };
 
-const getById = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const oneContact = await contactService.getContactById(contactId);
-    if (!oneContact) {
-      throw HttpError(404, `Contact with id=${id} not found`);
-    }
-    res.json(oneContact);
-  } catch (error) {
-    next(error);
+const getById = async (req, res) => {
+  const { contactId } = req.params;
+  const oneContact = await contactService.getContactById(contactId);
+  if (!oneContact) {
+    throw HttpError(404, `Contact with id=${id} not found`);
   }
+  res.json(oneContact);
 };
 
-const add = async (req, res, next) => {
-  try {
-    const { error } = addContactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, "Missing required name field");
-    }
-    const result = await contactService.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
+const add = async (req, res) => {
+  const result = await contactService.addContact(req.body);
+  res.status(201).json(result);
 };
 
-const updateById = async (req, res, next) => {
-  try {
-    const { error } = updateContactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, "Missing required name field");
-    }
-    const { contactId } = req.params;
-    const result = await contactService.updateContactById(contactId, req.body);
-    if (!result) {
-      throw HttpError(404, `Contact with id=${id} not found`);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+const updateById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contactService.updateContactById(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, `Contact with id=${id} not found`);
   }
+  res.json(result);
 };
 
-const deleteById = async (req, res, next) => {
+const deleteById = async (req, res) => {
   try {
     const { contactId } = req.params;
     const result = await contactService.removeContact(contactId);
@@ -74,9 +51,9 @@ const deleteById = async (req, res, next) => {
 };
 
 export default {
-  getAllContacts,
-  getById,
-  add,
-  updateById,
-  deleteById,
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updateById: ctrlWrapper(updateById),
+  deleteById: ctrlWrapper(deleteById),
 };
--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,6 +1,15 @@
 import express from "express";
+
 import contactsController from "../../controller/contacts-controller.js";
+
 import { isEmptyBody } from "../../middlewares/index.js";
+
+import { validatorBody } from "../../decorators/index.js";
+
+import {
+  addContactSchema,
+  updateContactSchema,
+} from "../../schemas/contacts-schema.js";
 
 const router = express.Router();
 
@@ -8,10 +17,20 @@ router.get("/", contactsController.getAllContacts);
 
 router.get("/:contactId", contactsController.getById);
 
-router.post("/", isEmptyBody, contactsController.add);
+router.post(
+  "/",
+  isEmptyBody,
+  validatorBody(addContactSchema),
+  contactsController.add
+);
 
 router.delete("/:contactId", contactsController.deleteById);
 
-router.put("/:contactId", isEmptyBody, contactsController.updateById);
+router.put(
+  "/:contactId",
+  isEmptyBody,
+  validatorBody(updateContactSchema),
+  contactsController.updateById
+);
 
 export default router;
