8ed0e348225c9792d97d2ebc2cc4e3bfe209d706
Ania <124797307+aniakurtiak@users.noreply.github.com>
2023-12-04 17:49:07 +0200

    Connect to DB

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+import mongoose from "mongoose";
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://AniaKurtiak:jG0gXax4r3DXpTkz@cluster0.pan0snk.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then((result) => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+      console.log("Database connection successful");
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
+
+// AniaKurtiak;
+// jG0gXax4r3DXpTkz;
