b8c7115bed6a74f88d4e756f10e0472ed3d0a514
Ania <124797307+aniakurtiak@users.noreply.github.com>
2023-12-09 20:38:25 +0200

    fix validateBody

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
+  const { status = err.status || 500, message = "Server error" } = err;
   res.status(status).json({
     message,
   })
