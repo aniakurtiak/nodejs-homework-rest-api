6f0d33c69b7d67cf57c809345542dd60b5451cfa
Ania <124797307+aniakurtiak@users.noreply.github.com>
2023-12-10 15:35:25 +0200

    fix messages

--- models/Contact.js
+++ models/Contact.js
@@ -33,13 +33,13 @@ const Contact = model("contact", contactSchema);
 
 export const addContactSchema = Joi.object({
   name: Joi.string().required().messages({
-    "any.required": `name must be exist`,
+    "any.required": `missing required name field`,
   }),
   email: Joi.string().required().messages({
-    "any.required": `email must be exist`,
+    "any.required": `missing required email field`,
   }),
   phone: Joi.string().required().messages({
-    "any.required": `phone must be exist`,
+    "any.required": `missing required phone field`,
   }),
   favorite: Joi.boolean(),
 });
